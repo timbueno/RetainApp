e923e2f60ee7534d850f0bf7eead592c051ee10c
Tim Bueno <tim@deadpan.io>
2023-04-11 13:00:28 -0400

    Initial Commit

---
+++ RetainApp/RetainAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  RetainAppApp.swift
+//  RetainApp
+//
+//  Created by Tim Bueno on 4/11/23.
+//
+
+import SwiftUI
+
+@main
+struct RetainAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
